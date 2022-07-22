ddce917e27ee566f71594e009a46dbfd28d966e5
GitHub Terraform <zscaler-github-P@zscaler.com>
2022-07-21 22:45:57 -0700

    Initial Commit

---
+++ zpa/services/applicationsegmentpra/service.go
@@ -0,0 +1,13 @@
+package applicationsegmentpra
+
+import (
+	"github.com/zscaler/zscaler-sdk-go/zpa"
+)
+
+type Service struct {
+	Client *zpa.Client
+}
+
+func New(c *zpa.Client) *Service {
+	return &Service{Client: c}
+}
