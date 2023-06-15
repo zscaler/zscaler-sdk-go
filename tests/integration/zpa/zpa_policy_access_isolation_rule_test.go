ba46d019edc44bf59c7deac7a467ab1b754fd307
GitHub Terraform <zscaler-github-P@zscaler.com>
2023-06-15 13:45:07 -0700

    feat: Added access policy tests

---
+++ tests/integration/zpa/zpa_policy_access_isolation_rule_test.go
@@ -0,0 +1,130 @@
+package integration
+
+/*
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/zscaler/zscaler-sdk-go/tests"
+	"github.com/zscaler/zscaler-sdk-go/zpa/services/isolationprofile"
+	"github.com/zscaler/zscaler-sdk-go/zpa/services/policysetcontroller"
+)
+
+func TestAccessIsolationPolicy(t *testing.T) {
+	policyType := "ISOLATION_POLICY"
+	isolationProfileID := "BD_SA_Profile1"
+	name := "tests-" + acctest.RandStringFromCharSet(10, acctest.CharSetAlpha)
+	updateName := "tests-" + acctest.RandStringFromCharSet(10, acctest.CharSetAlpha)
+	client, err := tests.NewZpaClient()
+	if err != nil {
+		t.Errorf("Error creating client: %v", err)
+		return
+	}
+
+	service := policysetcontroller.New(client)
+	accessPolicySet, _, err := service.GetByPolicyType(policyType)
+	if err != nil {
+		t.Errorf("Error getting access isolation policy set: %v", err)
+		return
+	}
+	profile := isolationprofile.New(client)
+	profileID, _, err := profile.GetByName(isolationProfileID)
+	if err != nil {
+		t.Errorf("Error getting isolation profile id set: %v", err)
+		return
+	}
+	accessPolicyRule := policysetcontroller.PolicyRule{
+		Name:                  name,
+		Description:           "New application segment",
+		RuleOrder:             "1",
+		PolicySetID:           accessPolicySet.ID,
+		ZpnIsolationProfileID: profileID.ID,
+		Action:                "ISOLATE",
+		Conditions:            []policysetcontroller.Conditions{
+		},
+	}
+	// Test resource creation
+	createdResource, _, err := service.Create(&accessPolicyRule)
+	// Check if the request was successful
+	if err != nil {
+		t.Errorf("Error making POST request: %v", err)
+	}
+
+	if createdResource.ID == "" {
+		t.Error("Expected created resource ID to be non-empty, but got ''")
+	}
+	if createdResource.Name != name {
+		t.Errorf("Expected created resource name '%s', but got '%s'", name, createdResource.Name)
+	}
+	// Test resource retrieval
+	retrievedResource, _, err := service.GetPolicyRule(accessPolicySet.ID, createdResource.ID)
+	if err != nil {
+		t.Errorf("Error retrieving resource: %v", err)
+	}
+	if retrievedResource.ID != createdResource.ID {
+		t.Errorf("Expected retrieved resource ID '%s', but got '%s'", createdResource.ID, retrievedResource.ID)
+	}
+	if retrievedResource.Name != name {
+		t.Errorf("Expected retrieved resource name '%s', but got '%s'", name, createdResource.Name)
+	}
+	// Test resource update
+	retrievedResource.Name = updateName
+	_, err = service.Update(accessPolicySet.ID, createdResource.ID, retrievedResource)
+	if err != nil {
+		t.Errorf("Error updating resource: %v", err)
+	}
+	updatedResource, _, err := service.GetPolicyRule(accessPolicySet.ID, createdResource.ID)
+	if err != nil {
+		t.Errorf("Error retrieving resource: %v", err)
+	}
+	if updatedResource.ID != createdResource.ID {
+		t.Errorf("Expected retrieved updated resource ID '%s', but got '%s'", createdResource.ID, updatedResource.ID)
+	}
+	if updatedResource.Name != updateName {
+		t.Errorf("Expected retrieved updated resource name '%s', but got '%s'", updateName, updatedResource.Name)
+	}
+	// Test resource retrieval by name
+	retrievedResource, _, err = service.GetByNameAndType(policyType, updateName)
+	if err != nil {
+		t.Errorf("Error retrieving resource by name: %v", err)
+	}
+	if retrievedResource.ID != createdResource.ID {
+		t.Errorf("Expected retrieved resource ID '%s', but got '%s'", createdResource.ID, retrievedResource.ID)
+	}
+	if retrievedResource.Name != updateName {
+		t.Errorf("Expected retrieved resource name '%s', but got '%s'", updateName, createdResource.Name)
+	}
+	// Test resources retrieval
+	resources, _, err := service.GetAllByType(policyType)
+	if err != nil {
+		t.Errorf("Error retrieving resources: %v", err)
+	}
+	if len(resources) == 0 {
+		t.Error("Expected retrieved resources to be non-empty, but got empty slice")
+	}
+	// check if the created resource is in the list
+	found := false
+	for _, resource := range resources {
+		if resource.ID == createdResource.ID {
+			found = true
+			break
+		}
+	}
+	if !found {
+		t.Errorf("Expected retrieved resources to contain created resource '%s', but it didn't", createdResource.ID)
+	}
+	// Test resource removal
+	_, err = service.Delete(accessPolicySet.ID, createdResource.ID)
+	if err != nil {
+		t.Errorf("Error deleting resource: %v", err)
+		return
+	}
+
+	// Test resource retrieval after deletion
+	_, _, err = service.GetPolicyRule(accessPolicySet.ID, createdResource.ID)
+	if err == nil {
+		t.Errorf("Expected error retrieving deleted resource, but got nil")
+	}
+
+}
+*/
