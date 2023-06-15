538cd4b47b8a0fd0bf0027cb36f9502af20ec6ee
GitHub Terraform <zscaler-github-P@zscaler.com>
2023-06-15 10:10:22 -0700

    feat: Added zpa integration tests

---
+++ tests/integration/zpa/zpa_ba_certificate_test.go
@@ -0,0 +1,39 @@
+package integration
+
+import (
+	"testing"
+
+	"github.com/zscaler/zscaler-sdk-go/tests"
+	"github.com/zscaler/zscaler-sdk-go/zpa/services/bacertificate"
+)
+
+func TestBACertificates(t *testing.T) {
+	client, err := tests.NewZpaClient()
+	if err != nil {
+		t.Errorf("Error creating client: %v", err)
+		return
+	}
+
+	service := bacertificate.New(client)
+
+	certificates, _, err := service.GetAll()
+	if err != nil {
+		t.Errorf("Error getting browser access certificates: %v", err)
+		return
+	}
+	if len(certificates) == 0 {
+		t.Errorf("No browser access certificate found")
+		return
+	}
+	name := certificates[0].Name
+	t.Log("Getting browser access certificate by name:" + name)
+	net, _, err := service.GetIssuedByName(name)
+	if err != nil {
+		t.Errorf("Error getting browser access certificate by name: %v", err)
+		return
+	}
+	if net.Name != name {
+		t.Errorf("browser access certificate name does not match: expected %s, got %s", name, net.Name)
+		return
+	}
+}
