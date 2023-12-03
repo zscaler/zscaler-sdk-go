e952acecf00a674b23985d93f6b635caac1c960f
William Guilherme <wguilherme@securitygeek.io>
2023-12-03 15:39:51 -0800

    feat: Added Sandbox Endpoint Resources

---
+++ zia/services/sandbox/sandbox_submission/sandbox_submission_test.go
@@ -0,0 +1,68 @@
+package sandbox_submission
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/zscaler/zscaler-sdk-go/v2/tests"
+)
+
+func TestSandboxSubmission(t *testing.T) {
+	client, err := tests.NewZiaClient()
+	if err != nil {
+		t.Fatalf("Error creating client: %v", err)
+	}
+	service := &Service{Client: client}
+
+	// Define the URL of the file to be tested
+	fileURL := "https://github.com/SecurityGeekIO/malware-samples/raw/main/test-pe-file.exe"
+
+	// Download the file
+	resp, err := http.Get(fileURL)
+	if err != nil {
+		t.Fatalf("Failed to download the test file: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("Failed to download the test file: HTTP status code %d", resp.StatusCode)
+	}
+
+	// Submit the file for scanning
+	scanResult, err := service.SubmitFile("test-pe-file.exe", resp.Body, "")
+	if err != nil {
+		t.Fatalf("Error submitting file for scanning: %v", err)
+	}
+
+	t.Logf("File submitted successfully: %+v", scanResult)
+}
+
+func TestSandboxDiscan(t *testing.T) {
+	client, err := tests.NewZiaClient()
+	if err != nil {
+		t.Fatalf("Error creating client: %v", err)
+	}
+	service := &Service{Client: client}
+
+	// Define the URL of the file to be tested
+	fileURL := "https://github.com/SecurityGeekIO/malware-samples/raw/main/test-pe-file.exe"
+
+	// Download the file
+	resp, err := http.Get(fileURL)
+	if err != nil {
+		t.Fatalf("Failed to download the test file: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("Failed to download the test file: HTTP status code %d", resp.StatusCode)
+	}
+
+	// Submit the file for scanning
+	scanResult, err := service.Discan("test-pe-file.exe", resp.Body)
+	if err != nil {
+		t.Fatalf("Error submitting file for scanning: %v", err)
+	}
+
+	t.Logf("File submitted successfully: %+v", scanResult)
+}
