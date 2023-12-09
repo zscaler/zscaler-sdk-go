51281bf3f084ecfe2955a5260a152d3708c8b719
William Guilherme <wguilherme@securitygeek.io>
2023-12-08 23:43:42 -0800

    (feat): Added more dlp integration test cases

--- zia/services/dlp/dlp_idm_profiles_lite/dlp_idm_profiles_lite.go
+++ zia/services/dlp/dlp_idm_profiles_lite/dlp_idm_profiles_lite.go
@@ -1,4 +1,4 @@
-package dlp_idm_profiles
+package dlp_idm_profile_lite
 
 import (
 	"fmt"
@@ -13,7 +13,6 @@ const (
 )
 
 // Gets a list of active IDM templates (or IDM profiles) and their criteria, only.
-
 type DLPIDMProfileLite struct {
 	// The identifier (1-64) for the IDM template (i.e., IDM profile) that is unique within the organization.
 	ProfileID int `json:"profileId,omitempty"`
@@ -34,9 +33,14 @@ type DLPIDMProfileLite struct {
 	ModifiedBy *common.IDNameExtensions `json:"modifiedBy,omitempty"`
 }
 
-func (service *Service) GetDLPProfileLiteID(ProfileLiteID int) (*DLPIDMProfileLite, error) {
+func (service *Service) GetDLPProfileLiteID(ProfileLiteID int, activeOnly bool) (*DLPIDMProfileLite, error) {
+	endpoint := fmt.Sprintf("%s/%d", dlpIDMProfileLiteEndpoint, ProfileLiteID)
+	if activeOnly {
+		endpoint += "?activeOnly=true"
+	}
+
 	var profileLite DLPIDMProfileLite
-	err := service.Client.Read(fmt.Sprintf("%s/%d", dlpIDMProfileLiteEndpoint, ProfileLiteID), &profileLite)
+	err := service.Client.Read(endpoint, &profileLite)
 	if err != nil {
 		return nil, err
 	}
@@ -45,16 +49,34 @@ func (service *Service) GetDLPProfileLiteID(ProfileLiteID int) (*DLPIDMProfileLi
 	return &profileLite, nil
 }
 
-func (service *Service) GetDLPProfileLiteByName(profileLiteName string) (*DLPIDMProfileLite, error) {
+func (service *Service) GetDLPProfileLiteByName(profileLiteName string, activeOnly bool) (*DLPIDMProfileLite, error) {
+	queryParameters := url.Values{}
+	queryParameters.Set("name", profileLiteName)
+	if activeOnly {
+		queryParameters.Set("activeOnly", "true")
+	}
+
+	endpoint := fmt.Sprintf("%s?%s", dlpIDMProfileLiteEndpoint, queryParameters.Encode())
 	var profileLite []DLPIDMProfileLite
-	err := common.ReadAllPages(service.Client, fmt.Sprintf("%s?name=%s", dlpIDMProfileLiteEndpoint, url.QueryEscape(profileLiteName)), &profileLite)
+	err := common.ReadAllPages(service.Client, endpoint, &profileLite)
 	if err != nil {
 		return nil, err
 	}
-	for _, profileLite := range profileLite {
-		if strings.EqualFold(profileLite.TemplateName, profileLiteName) {
-			return &profileLite, nil
+	for _, profile := range profileLite {
+		if strings.EqualFold(profile.TemplateName, profileLiteName) {
+			return &profile, nil
 		}
 	}
 	return nil, fmt.Errorf("no idm profile template found with name: %s", profileLiteName)
+}
+
+func (service *Service) GetAll(activeOnly bool) ([]DLPIDMProfileLite, error) {
+	endpoint := dlpIDMProfileLiteEndpoint
+	if activeOnly {
+		endpoint += "?activeOnly=true"
+	}
+
+	var idmpProfile []DLPIDMProfileLite
+	err := common.ReadAllPages(service.Client, endpoint, &idmpProfile)
+	return idmpProfile, err
 }
