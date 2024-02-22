27237db7e7c924f8281d5e9958ad8963303ee454
William Guilherme <wguilherme@securitygeek.io>
2024-02-22 14:06:03 -0800

    feat: Added Emergency Access and Okta Test

--- zpa/services/emergencyaccess/emergencyaccess.go
+++ zpa/services/emergencyaccess/emergencyaccess.go
@@ -3,34 +3,31 @@ package emergencyaccess
 import (
 	"fmt"
 	"net/http"
-	"net/url"
-	"strconv"
-
-	"github.com/zscaler/zscaler-sdk-go/v2/zpa/services/common"
 )
 
 const (
 	mgmtConfig              = "/mgmtconfig/v1/admin/customers/"
-	emergencyAccessEndpoint = "/emergencyAccess/users"
+	emergencyAccessEndpoint = "/emergencyAccess/user"
 )
 
 type EmergencyAccess struct {
 	ActivatedOn       string `json:"activatedOn,omitempty"`
 	AllowedActivate   bool   `json:"allowedActivate"`
 	AllowedDeactivate bool   `json:"allowedDeactivate"`
-	EmailId           string `json:"emailId,omitempty"`
+	EmailID           string `json:"emailId,omitempty"`
 	FirstName         string `json:"firstName,omitempty"`
 	LastLoginTime     string `json:"lastLoginTime,omitempty"`
 	LastName          string `json:"lastName,omitempty"`
 	UpdateEnabled     bool   `json:"updateEnabled"`
-	UserId            string `json:"userId,omitempty"`
+	UserID            string `json:"userId,omitempty"`
 	UserStatus        string `json:"userStatus,omitempty"`
+	ActivateNow       bool   `json:"activateNow,omitempty" url:"activateNow,omitempty"`
 }
 
-func (service *Service) Get(userID string) (*EmergencyAccess, *http.Response, error) {
+func (service *Service) Get(profileID string) (*EmergencyAccess, *http.Response, error) {
 	v := new(EmergencyAccess)
-	relativeURL := fmt.Sprintf("%s/%s", mgmtConfig+service.Client.Config.CustomerID+emergencyAccessEndpoint, userID)
-	resp, err := service.Client.NewRequestDo("GET", relativeURL, common.Filter{MicroTenantID: service.microTenantID}, nil, v)
+	relativeURL := fmt.Sprintf("%s/%s", mgmtConfig+service.Client.Config.CustomerID+emergencyAccessEndpoint, profileID)
+	resp, err := service.Client.NewRequestDo("GET", relativeURL, nil, nil, &v)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -38,13 +35,33 @@ func (service *Service) Get(userID string) (*EmergencyAccess, *http.Response, er
 	return v, resp, nil
 }
 
-// POST - /mgmtconfig/v1/admin/customers/{customerId}/emergencyAccess/user
-func (service *Service) Create(emergencyAccess EmergencyAccess, activateNow bool) (*EmergencyAccess, *http.Response, error) {
+// func (service *Service) GetByEmailID(emailID string) (*EmergencyAccess, *http.Response, error) {
+// 	// Use the GetAll function to retrieve all EmergencyAccess records
+// 	list, resp, err := service.GetAll()
+// 	if err != nil {
+// 		return nil, nil, err
+// 	}
+
+// 	// Filter the retrieved list for the specific emailID
+// 	for _, emgAccess := range list {
+// 		if strings.EqualFold(emgAccess.EmailId, emailID) {
+// 			return &emgAccess, resp, nil
+// 		}
+// 	}
+
+// 	// If no matching record is found, return an error
+// 	return nil, resp, fmt.Errorf("no emergency access record found with email ID '%s'", emailID)
+// }
+
+func (service *Service) Create(emergencyAccess *EmergencyAccess) (*EmergencyAccess, *http.Response, error) {
+	// Constructing the relative URL correctly
+	emergencyAccess.ActivateNow = false // Ensure this parameter is set as intended
+	// Constructing the relative URL correctly
+	relativeURL := fmt.Sprintf("%s%s%s", mgmtConfig, service.Client.Config.CustomerID, emergencyAccessEndpoint)
 	v := new(EmergencyAccess)
-	queryParams := url.Values{}
-	queryParams.Set("activateNow", strconv.FormatBool(activateNow)) // Adding activateNow as a query parameter
-	relativeURL := fmt.Sprintf("%s/%s%s?%s", mgmtConfig, service.Client.Config.CustomerID, emergencyAccessEndpoint, queryParams.Encode())
-	resp, err := service.Client.NewRequestDo("POST", relativeURL, common.Filter{MicroTenantID: service.microTenantID}, emergencyAccess, v)
+
+	// Making the request without common.Filter
+	resp, err := service.Client.NewRequestDo("POST", relativeURL, nil, emergencyAccess, v)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -52,9 +69,9 @@ func (service *Service) Create(emergencyAccess EmergencyAccess, activateNow bool
 	return v, resp, nil
 }
 
-func (service *Service) Update(userID string, emergencyAccess EmergencyAccess) (*http.Response, error) {
-	path := fmt.Sprintf("%s/%s", mgmtConfig+service.Client.Config.CustomerID+emergencyAccessEndpoint, userID)
-	resp, err := service.Client.NewRequestDo("PUT", path, common.Filter{MicroTenantID: service.microTenantID}, emergencyAccess, nil)
+func (service *Service) Update(userID string, emergencyAccess *EmergencyAccess) (*http.Response, error) {
+	path := fmt.Sprintf("%v/%v", mgmtConfig+service.Client.Config.CustomerID+emergencyAccessEndpoint, userID)
+	resp, err := service.Client.NewRequestDo("PUT", path, nil, emergencyAccess, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -64,7 +81,9 @@ func (service *Service) Update(userID string, emergencyAccess EmergencyAccess) (
 // PUT - /mgmtconfig/v1/admin/customers/{customerId}/emergencyAccess/user/{userId}/activate
 func (service *Service) Activate(userID string) (*http.Response, error) {
 	path := fmt.Sprintf("%s/%s/activate", mgmtConfig+service.Client.Config.CustomerID+emergencyAccessEndpoint, userID)
-	resp, err := service.Client.NewRequestDo("PUT", path, common.Filter{MicroTenantID: service.microTenantID}, nil, nil)
+
+	// Making the request without common.Filter
+	resp, err := service.Client.NewRequestDo("PUT", path, nil, nil, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -74,18 +93,72 @@ func (service *Service) Activate(userID string) (*http.Response, error) {
 // PUT - /mgmtconfig/v1/admin/customers/{customerId}/emergencyAccess/user/{userId}/deactivate
 func (service *Service) Deactivate(userID string) (*http.Response, error) {
 	path := fmt.Sprintf("%s/%s/deactivate", mgmtConfig+service.Client.Config.CustomerID+emergencyAccessEndpoint, userID)
-	resp, err := service.Client.NewRequestDo("PUT", path, common.Filter{MicroTenantID: service.microTenantID}, nil, nil)
+
+	// Making the request without common.Filter
+	resp, err := service.Client.NewRequestDo("PUT", path, nil, nil, nil)
 	if err != nil {
 		return nil, err
 	}
 	return resp, nil
 }
 
-func (service *Service) GetAll() ([]EmergencyAccess, *http.Response, error) {
-	relativeURL := mgmtConfig + service.Client.Config.CustomerID + emergencyAccessEndpoint
-	list, resp, err := common.GetAllPagesGenericWithCustomFilters[EmergencyAccess](service.Client, relativeURL, common.Filter{MicroTenantID: service.microTenantID})
-	if err != nil {
-		return nil, nil, err
-	}
-	return list, resp, nil
-}
+// func (service *Service) GetAll() ([]EmergencyAccess, *http.Response, error) {
+// 	relativeURL := fmt.Sprintf("%s%s%s", mgmtConfig, service.Client.Config.CustomerID, emergencyAccessEndpoint+"s") // Correct endpoint
+// 	pageSize := 20                                                                                                  // Define the pageSize as needed
+// 	initialPageId := ""                                                                                             // Start without a pageId or as required
+
+// 	return GetAllEmergencyAccessUsers(service.Client, relativeURL, pageSize, initialPageId)
+// }
+
+// func fetchEmergencyAccessUsersPage(client *zpa.Client, fullURL string) (*http.Response, error) {
+// 	// Directly use the fullURL, which is expected to be correctly formatted beforehand
+// 	return client.NewRequestDo("GET", fullURL, nil, nil, nil)
+// }
+
+// func GetAllEmergencyAccessUsers(client *zpa.Client, baseRelativeURL string, pageSize int, initialPageId string) ([]EmergencyAccess, *http.Response, error) {
+// 	var allUsers []EmergencyAccess
+// 	var lastResponse *http.Response
+// 	pageId := initialPageId
+
+// 	for {
+// 		// Construct the URL for each request to avoid duplication and encoding issues
+// 		var fullURL string
+// 		if pageId != "" {
+// 			fullURL = fmt.Sprintf("%s?pageSize=%d&pageId=%s", baseRelativeURL, pageSize, pageId)
+// 		} else {
+// 			fullURL = fmt.Sprintf("%s?pageSize=%d", baseRelativeURL, pageSize)
+// 		}
+
+// 		resp, err := fetchEmergencyAccessUsersPage(client, fullURL)
+// 		if err != nil {
+// 			return nil, lastResponse, err
+// 		}
+
+// 		// Assume this struct matches the expected JSON response structure
+// 		var pageData struct {
+// 			Items    []EmergencyAccess `json:"items"`
+// 			NextPage string            `json:"nextPage"`
+// 		}
+
+// 		bodyBytes, err := ioutil.ReadAll(resp.Body)
+// 		resp.Body.Close() // Ensure the body is always closed after reading
+// 		if err != nil {
+// 			return nil, resp, fmt.Errorf("error reading response body: %w", err)
+// 		}
+
+// 		if err := json.Unmarshal(bodyBytes, &pageData); err != nil {
+// 			return nil, resp, fmt.Errorf("error unmarshalling response: %w", err)
+// 		}
+
+// 		allUsers = append(allUsers, pageData.Items...)
+// 		if pageData.NextPage == "" {
+// 			break // Exit the loop if there are no more pages
+// 		}
+
+// 		// Update pageId for the next iteration
+// 		pageId = pageData.NextPage
+// 		lastResponse = resp
+// 	}
+
+// 	return allUsers, lastResponse, nil
+// }
--- zpa/services/emergencyaccess/emergencyaccess_test.go
+++ zpa/services/emergencyaccess/emergencyaccess_test.go
@@ -1,83 +1,112 @@
 package emergencyaccess
 
 import (
+	"context"
+	"fmt"
+	"os"
 	"testing"
+	"time"
 
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/okta/okta-sdk-golang/v2/okta"
+	"github.com/stretchr/testify/assert"
 	"github.com/zscaler/zscaler-sdk-go/v2/tests"
 )
 
-func TestEmergencyAccess(t *testing.T) {
+func TestEmergencyAccessIntegration(t *testing.T) {
+	randomName := acctest.RandStringFromCharSet(10, acctest.CharSetAlpha)
+
 	client, err := tests.NewZpaClient()
 	if err != nil {
 		t.Errorf("Error creating client: %v", err)
 		return
 	}
-
 	service := New(client)
 
 	// Create new resource
-	createdResource := EmergencyAccess{
+	createdResource, _, err := service.Create(&EmergencyAccess{
 		ActivatedOn:       "1",
 		AllowedActivate:   true,
-		AllowedDeactivate: false,
-		EmailId:           "jdoe@bd-hashicorp.com",
+		AllowedDeactivate: true,
+		EmailID:           randomName + "@bd-hashicorp.com",
 		FirstName:         "John",
-		LastName:          "Doe",
-		UserId:            "jdoe",
-	}
-
+		LastName:          "Smith",
+		UserID:            "jsmith",
+	})
 	if err != nil {
-		t.Fatalf("Error creating resource: %v", err)
+		t.Fatalf("Failed to create emergency user: %v", err)
 	}
 
-	t.Run("TestResourceCreation", func(t *testing.T) {
-		if createdResource.EmailId == "" {
-			t.Error("Expected created resource ID to be non-empty, but got ''")
-		}
+	// Test Get
+	gotResource, _, err := service.Get(createdResource.UserID)
+	if err != nil {
+		t.Errorf("Failed to get emergency user by UserID: %v", err)
+	}
+	assert.Equal(t, createdResource.UserID, gotResource.UserID, "UserID does not match")
 
-	})
+	//Test Update
+	updatedResource := *createdResource
+	updatedResource.FirstName = randomName
+	_, err = service.Update(createdResource.UserID, &updatedResource)
+	if err != nil {
+		t.Errorf("Failed to update emergency user: %v", err)
+	}
 
-	t.Run("TestResourceRetrieval", func(t *testing.T) {
-		retrievedResource, _, err := service.Get(createdResource.EmailId)
-		if err != nil {
-			t.Fatalf("Error retrieving resource: %v", err)
-		}
-		if retrievedResource.EmailId != createdResource.EmailId {
-			t.Errorf("Expected retrieved resource ID '%s', but got '%s'", createdResource.EmailId, retrievedResource.EmailId)
-		}
+	// Verify Update
+	updated, _, err := service.Get(createdResource.UserID)
+	if err != nil {
+		t.Errorf("Failed to get updated emergency user: %v", err)
+	}
+	assert.Equal(t, randomName, updated.FirstName, "FirstName was not updated")
 
-	})
+	// Test Emergency Access User Deactivation
+	_, err = service.Deactivate(createdResource.UserID)
+	if err != nil {
+		t.Errorf("Failed to deactivate emergency user: %v", err)
+	}
 
-	t.Run("TestAllResourcesRetrieval", func(t *testing.T) {
-		resources, _, err := service.GetAll()
-		if err != nil {
-			t.Fatalf("Error retrieving groups: %v", err)
-		}
-		if len(resources) == 0 {
-			t.Error("Expected retrieved resources to be non-empty, but got empty slice")
-		}
-		found := false
-		for _, resource := range resources {
-			if resource.EmailId == createdResource.EmailId {
-				found = true
-				break
-			}
-		}
-		if !found {
-			t.Errorf("Expected retrieved groups to contain created resource '%s', but it didn't", createdResource.EmailId)
-		}
-	})
+	// Test Emergency Access User Activate
+	_, err = service.Activate(createdResource.UserID)
+	if err != nil {
+		t.Errorf("Failed to activate emergency user: %v", err)
+	}
+
+	// Test Emergency Access User Deactivation
+	_, err = service.Deactivate(createdResource.UserID)
+	if err != nil {
+		t.Errorf("Failed to deactivate emergency user: %v", err)
+	}
+
+	// Simulate delay after deactivation in Cloud Service 1 before proceeding to Okta deletion
+	time.Sleep(10 * time.Second) // Adjust the delay as necessary
+
+	// Begin Okta deletion process
+	deleteUserInOkta(t, []string{createdResource.UserID}) // Passing the UserID to be deleted in Okta
 }
 
-func TestRetrieveNonExistentResource(t *testing.T) {
-	client, err := tests.NewZpaClient()
-	if err != nil {
-		t.Fatalf("Error creating client: %v", err)
-	}
-	service := New(client)
+// deleteUserInOkta deletes a user (or users) in Okta based on provided user IDs
+func deleteUserInOkta(t *testing.T, userIDs []string) {
+	// Fetch Okta domain and API token from environment variables
+	oktaDomain := os.Getenv("OKTA_DOMAIN")
+	apiToken := os.Getenv("OKTA_API_TOKEN")
 
-	_, _, err = service.Get("non-existent-id")
-	if err == nil {
-		t.Error("Expected error retrieving non-existent resource, but got nil")
+	// Initialize Okta client with environment variables
+	ctx, client, err := okta.NewClient(
+		context.TODO(),
+		okta.WithOrgUrl(fmt.Sprintf("https://%s", oktaDomain)),
+		okta.WithToken(apiToken),
+	)
+	if err != nil {
+		t.Errorf("Error initializing Okta client: %v", err)
+		return
+	}
+
+	for _, userID := range userIDs {
+		_, err := client.User.DeactivateOrDeleteUser(ctx, userID, nil)
+		if err != nil {
+			t.Errorf("Failed to delete user %s in Okta: %v", userID, err)
+		} else {
+			fmt.Printf("User %s deleted successfully in Okta\n", userID)
+		}
 	}
 }
