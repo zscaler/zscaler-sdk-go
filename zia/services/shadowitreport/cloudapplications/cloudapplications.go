bd96a4fef831613dee5aa31f3d390410929e3596
William Guilherme <wguilherme@securitygeek.io>
2024-07-05 21:30:46 -0700

    feat: Added ZIA Shadow IT Report Endpoints

---
+++ zia/services/shadowitreport/cloudapplications/cloudapplications.go
@@ -0,0 +1,220 @@
+package cloudapplications
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+
+	"github.com/zscaler/zscaler-sdk-go/v2/zia/services"
+	"github.com/zscaler/zscaler-sdk-go/v2/zia/services/common"
+)
+
+const (
+	cloudApplicationsEndpoint    = "/cloudApplications/lite"
+	customTagsEndpoint           = "/customTags"
+	appExportEndpoint            = "/shadowIT/applications/export"
+	appExportCsvEndpointTemplate = "/shadowIT/applications/%s/exportCsv"
+	appBulkUpdateEndpoint        = "/cloudApplications/bulkUpdate"
+)
+
+type CloudApplicationsAndCustomTags struct {
+	// Unique identifier of the cloud application
+	ID int `json:"id"`
+
+	// The name of the cloud application
+	Name string `json:"name,omitempty"`
+}
+
+type CloudApplicationsExport struct {
+	Duration                         string                   `json:"duration,omitempty"`
+	Application                      []string                 `json:"application,omitempty"`
+	AppName                          string                   `json:"appName,omitempty"`
+	ApplicationCategory              []string                 `json:"applicationCategory,omitempty"`
+	DataConsumed                     []common.DataConsumed    `json:"dataConsumed,omitempty"`
+	RiskIndex                        []int                    `json:"riskIndex,omitempty"`
+	Order                            *common.Order            `json:"order,omitempty"`
+	SanctionedState                  []string                 `json:"sanctionedState,omitempty"`
+	Employees                        []string                 `json:"employees,omitempty"`
+	SupportedCertifications          *SupportedCertifications `json:"supportedCertifications,omitempty"`
+	SourceIpRestriction              []string                 `json:"sourceIpRestriction,omitempty"`
+	MfaSupport                       []string                 `json:"mfaSupport,omitempty"`
+	AdminAuditLogs                   []string                 `json:"adminAuditLogs,omitempty"`
+	HadBreachInLast3Years            []string                 `json:"hadBreachInLast3Years,omitempty"`
+	HavePoorItemsOfService           []string                 `json:"havePoorItemsOfService,omitempty"`
+	PasswordStrength                 []string                 `json:"passwordStrength,omitempty"`
+	SslPinned                        []string                 `json:"sslPinned,omitempty"`
+	Evasive                          []string                 `json:"evasive,omitempty"`
+	HaveHTTPSecurityHeaderSupport    []string                 `json:"haveHTTPSecurityHeaderSupport,omitempty"`
+	DnsCAAPolicy                     []string                 `json:"dnsCAAPolicy,omitempty"`
+	HaveWeakCipherSupport            []string                 `json:"haveWeakCipherSupport,omitempty"`
+	SslCertificationValidity         []string                 `json:"sslCertificationValidity,omitempty"`
+	MalwareScanningContent           []string                 `json:"malwareScanningContent,omitempty"`
+	FileSharing                      []string                 `json:"fileSharing,omitempty"`
+	RemoteAccessScreenSharing        []string                 `json:"remoteAccessScreenSharing,omitempty"`
+	SenderPolicyFramework            []string                 `json:"senderPolicyFramework,omitempty"`
+	DomainKeysIdentifiedMail         []string                 `json:"domainKeysIdentifiedMail,omitempty"`
+	DomainBasedMessageAuthentication []string                 `json:"domainBasedMessageAuthentication,omitempty"`
+	VulnerableDisclosureProgram      []string                 `json:"vulnerableDisclosureProgram,omitempty"`
+	WafSupport                       []string                 `json:"wafSupport,omitempty"`
+	Vulnerability                    []string                 `json:"vulnerability,omitempty"`
+	ValidSSLCertificate              []string                 `json:"validSSLCertificate,omitempty"`
+	DataEncryptionInTransit          []string                 `json:"dataEncryptionInTransit,omitempty"`
+	VulnerableToHeartBleed           []string                 `json:"vulnerableToHeartBleed,omitempty"`
+	VulnerableToPoodle               []string                 `json:"vulnerableToPoodle,omitempty"`
+	VulnerableToLogJam               []string                 `json:"vulnerableToLogJam,omitempty"`
+	CertKeySize                      *CertKeySize             `json:"certKeySize,omitempty"`
+}
+
+type SupportedCertifications struct {
+	Operation string   `json:"operation,omitempty"`
+	Value     []string `json:"value,omitempty"`
+}
+
+type CertKeySize struct {
+	Operation string   `json:"operation,omitempty"`
+	Value     []string `json:"value,omitempty"`
+}
+
+type CloudApplicationsExportCSV struct {
+	Duration      string                `json:"duration,omitempty"`
+	Application   []string              `json:"application,omitempty"`
+	Order         *common.Order         `json:"order,omitempty"`
+	DownloadBytes []common.DataConsumed `json:"downloadBytes,omitempty"`
+	UploadBytes   []common.DataConsumed `json:"uploadBytes,omitempty"`
+	DataConsumed  []common.DataConsumed `json:"dataConsumed,omitempty"`
+	Users         []User                `json:"users,omitempty"`
+	Locations     []Location            `json:"locations,omitempty"`
+	Departments   []Department          `json:"departments,omitempty"`
+}
+
+type User struct {
+	ID          int    `json:"id,omitempty"`
+	PID         int    `json:"pid,omitempty"`
+	Name        string `json:"name,omitempty"`
+	Description string `json:"description,omitempty"`
+	Deleted     bool   `json:"deleted,omitempty"`
+	GetlID      int    `json:"getlId,omitempty"`
+}
+
+type Location struct {
+	ID          int    `json:"id,omitempty"`
+	PID         int    `json:"pid,omitempty"`
+	Name        string `json:"name,omitempty"`
+	Description string `json:"description,omitempty"`
+	Deleted     bool   `json:"deleted,omitempty"`
+	GetlID      int    `json:"getlId,omitempty"`
+}
+
+type Department struct {
+	ID          int    `json:"id,omitempty"`
+	PID         int    `json:"pid,omitempty"`
+	Name        string `json:"name,omitempty"`
+	Description string `json:"description,omitempty"`
+	Deleted     bool   `json:"deleted,omitempty"`
+	GetlID      int    `json:"getlId,omitempty"`
+}
+
+type ApplicationBulkUpdate struct {
+	SanctionedState                string                           `json:"sanctionedState,omitempty"`
+	ApplicationIDs                 []int                            `json:"applicationIds,omitempty"`
+	CloudApplicationsAndCustomTags []CloudApplicationsAndCustomTags `json:"customTags,omitempty"`
+}
+
+// GetAllCloudAppsLite retrieves all cloud applications in lite format with optional pagination parameters
+func GetAllCloudAppsLite(service *services.Service, pageNumber, limit *int) ([]CloudApplicationsAndCustomTags, error) {
+	endpoint := cloudApplicationsEndpoint
+
+	// Build the query parameters
+	queryParams := url.Values{}
+	if pageNumber != nil {
+		queryParams.Add("PageNumber", strconv.Itoa(*pageNumber))
+	}
+	if limit != nil {
+		queryParams.Add("limit", strconv.Itoa(*limit))
+	}
+
+	// Append query parameters to endpoint if any
+	if len(queryParams) > 0 {
+		endpoint += "?" + queryParams.Encode()
+	}
+
+	var cloudAppsLite []CloudApplicationsAndCustomTags
+	err := service.Client.Read(endpoint, &cloudAppsLite)
+	if err != nil {
+		return nil, err
+	}
+
+	return cloudAppsLite, nil
+}
+
+func GetAllCustomTags(service *services.Service) ([]CloudApplicationsAndCustomTags, error) {
+	var customTags []CloudApplicationsAndCustomTags
+	err := service.Client.Read(customTagsEndpoint, &customTags)
+	return customTags, err
+}
+
+// CreateCloudApplicationsExport sends a POST request to create a new CloudApplicationsExport
+func CreateCloudApplicationsExport(service *services.Service, entity string, appExport *CloudApplicationsExport) (*CloudApplicationsExport, *http.Response, error) {
+	// Validate the entity parameter
+	if entity != "USER" && entity != "LOCATION" {
+		return nil, nil, errors.New("invalid entity value; must be 'USER' or 'LOCATION'")
+	}
+	// Create a map to include the entity and the CloudApplicationsExport
+	payload := map[string]interface{}{
+		"entity":  entity,
+		"payload": appExport,
+	}
+	resp, err := service.Client.Create(appExportEndpoint, payload)
+	if err != nil {
+		return nil, nil, err
+	}
+	createdExport, ok := resp.(*CloudApplicationsExport)
+	if !ok {
+		return nil, nil, errors.New("object returned from API was not a CloudApplicationsExport pointer")
+	}
+	service.Client.Logger.Printf("[DEBUG] Successfully created new application export")
+	return createdExport, nil, nil
+}
+
+// CreateCloudApplicationsExportCSV sends a POST request to create a new CloudApplicationsExportCSV
+// CreateCloudApplicationsExportCSV sends a POST request to create a new CloudApplicationsExportCSV
+func CreateCloudApplicationsExportCSV(service *services.Service, entity string, appExport *CloudApplicationsExportCSV) (*CloudApplicationsExportCSV, *http.Response, error) {
+	// Validate the entity parameter
+	if entity != "USER" && entity != "LOCATION" {
+		return nil, nil, errors.New("invalid entity value; must be 'USER' or 'LOCATION'")
+	}
+
+	// Create the endpoint with the entity
+	endpoint := fmt.Sprintf(appExportCsvEndpointTemplate, entity)
+
+	resp, err := service.Client.Create(endpoint, appExport)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	createdExport, ok := resp.(*CloudApplicationsExportCSV)
+	if !ok {
+		return nil, nil, errors.New("object returned from API was not a CloudApplicationsExportCSV pointer")
+	}
+
+	service.Client.Logger.Printf("[DEBUG] Successfully created new application export")
+	return createdExport, nil, nil
+}
+
+// Update sends a PUT request to perform bulk updates for cloud applications
+func Update(service *services.Service, rules *ApplicationBulkUpdate) (*ApplicationBulkUpdate, error) {
+	resp, err := service.Client.UpdateWithPut(appBulkUpdateEndpoint, *rules)
+	if err != nil {
+		return nil, err
+	}
+
+	updatedRules, ok := resp.(*ApplicationBulkUpdate)
+	if !ok {
+		return nil, errors.New("object returned from API was not an ApplicationBulkUpdate pointer")
+	}
+
+	service.Client.Logger.Printf("[DEBUG] Successfully updated application bulk update")
+	return updatedRules, nil
+}
---
+++ zia/services/shadowitreport/cloudapplications/cloudapplications_test.go
@@ -0,0 +1,39 @@
+package cloudapplications
+
+import (
+	"testing"
+
+	"github.com/zscaler/zscaler-sdk-go/v2/tests"
+	"github.com/zscaler/zscaler-sdk-go/v2/zia/services"
+)
+
+func TestGetAllCloudAppsLite(t *testing.T) {
+	client, err := tests.NewZiaClient()
+	if err != nil {
+		t.Fatalf("Error creating client: %v", err)
+	}
+
+	service := services.New(client)
+
+	// Retrieve all cloud applications without optional parameters
+	apps, err := GetAllCloudAppsLite(service, nil, nil)
+	if err != nil {
+		t.Fatalf("Error getting all cloud applications: %v", err)
+	}
+	if len(apps) == 0 {
+		t.Log("No cloud applications found")
+		return
+	}
+
+	// Retrieve all cloud applications with optional parameters
+	pageNumber := 1
+	limit := 10
+	apps, err = GetAllCloudAppsLite(service, &pageNumber, &limit)
+	if err != nil {
+		t.Fatalf("Error getting all cloud applications with pagination: %v", err)
+	}
+	if len(apps) == 0 {
+		t.Log("No cloud applications found with pagination")
+		return
+	}
+}
