4c01040d3c31e8d266e8b591292aa46f639dad71
William Guilherme <wguilherme@securitygeek.io>
2025-03-17 14:54:50 -0700

    fix: Fixed ZPA URL Encoding

--- zscaler/zpa/services/common/common.go
+++ zscaler/zpa/services/common/common.go
@@ -361,9 +361,22 @@ func GetAllPagesScimGenericWithSearch[T any](
 // 	return strings.TrimSpace(query)
 // }
 
+// func sanitizeSearchQuery(query string) string {
+// 	// Remove special characters except spaces, alphanumeric characters, and dashes
+// 	re := regexp.MustCompile(`[^a-zA-Z0-9\s-]`)
+// 	query = re.ReplaceAllString(query, "")
+
+// 	// Replace multiple spaces with a single space
+// 	reSpace := regexp.MustCompile(`\s+`)
+// 	query = reSpace.ReplaceAllString(query, " ")
+
+// 	// Trim spaces (but do NOT encode again)
+// 	return strings.TrimSpace(query)
+// }
+
 func sanitizeSearchQuery(query string) string {
-	// Remove special characters except spaces, alphanumeric characters, and dashes
-	re := regexp.MustCompile(`[^a-zA-Z0-9\s-]`)
+	// Remove special characters except spaces, alphanumeric characters, dashes, and underscores
+	re := regexp.MustCompile(`[^a-zA-Z0-9\s_\-]`)
 	query = re.ReplaceAllString(query, "")
 
 	// Replace multiple spaces with a single space
